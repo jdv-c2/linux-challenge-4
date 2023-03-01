af2d02971fb785b7d4a2d343724c43067fb361ab
Johan de Vries <johandevries@c2-academy.com>
2023-03-01 21:41:09 +0000

    Add ticket-scanner.c

---
+++ c/ticket-scanner.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#define MAX_TICKET_LENGTH 20
+#define PATH "/usr/share/.linux-adventures/"
+
+int main(){
+   char ticket[MAX_TICKET_LENGTH];
+   char correct_ticket[] = "2023-CI-CY-219";
+   
+   if (getuid() == 6543) {
+      printf("Doorman!");
+      printf("Enter your password: ");
+      fgets(password, MAX_TICKET_LENGTH, stdin);
+
+      password[strcspn(password, "\n")] = 0;
+
+      if (strcmp(password, correct_password) == 0) {
+         printf("Welcome!\n");
+         system("/bin/cat " PATH "badge/badge"); 
+      } else {
+         printf("Invalid ticket. Try again.\n");
+      }
+   } else {
+      printf("Only the doorman can operate the ticket scanner!");
+   }
+
+   return 0;
+}
