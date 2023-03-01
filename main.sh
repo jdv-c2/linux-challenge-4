6937e734fe1c6a780a8319c6edf65ba93778d42c
Johan de Vries <johandevries@c2-academy.com>
2023-03-01 23:37:24 +0000

    Fix cp command

--- main.sh
+++ main.sh
@@ -115,11 +115,11 @@ main()
    chown doorman_x:circus_cyber /birthday/grand-ballroom/ticket-scanner
 
    # Mission 2
-   cp /hotel/table-3 /birthday/grand-ballroom/table-3
+   cp hotel/table-3 /birthday/grand-ballroom/table-3
    chown guest_3: /birthday/grand-ballroom/table-3
    echo "The password of the elephant is: w7GTfP2z" > /birthday/floor-2/room-208/.secret 
-   chown guest_3: /birthday/floor-2/room-208/.secret.secret
-   chmod 000 /birthday/floor-2/room-208/.secret.secret
+   chown guest_3: /birthday/floor-2/room-208/.secret
+   chmod 000 /birthday/floor-2/room-208/.secret
 
 }
 
